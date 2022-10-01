60ecbbabc5c4fa56040735700340949f8eecb063
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-10-01 16:52:55 +0300

    p10k bar color changed

--- .p10k.zsh
+++ .p10k.zsh
@@ -162,7 +162,7 @@
   fi
 
   # Default background color.
-  typeset -g POWERLEVEL9K_BACKGROUND=236
+  typeset -g POWERLEVEL9K_BACKGROUND=234
 
   # Separator between same-color segments on the left.
   typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='%244F\uE0B1'
